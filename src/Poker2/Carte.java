75843338278dd4198218095ff0bc558faecb9daf
GabrielPlante <gabriel.plante@live.fr>
2018-09-26 16:00:09 +0200

    Pre-slice1

---
+++ src/Poker2/Carte.java
@@ -0,0 +1,11 @@
+package Poker2;
+
+public class Carte {
+    public Carte(int a, String b)
+    {
+        nombre = a;
+        couleur = b;
+    }
+    public int nombre;
+    public String couleur;
+}
---
+++ src/Poker2/pokerMain.java
@@ -0,0 +1,7 @@
+package Poker2;
+
+public class pokerMain {
+    public static void main(String[] args) {
+
+    }
+}

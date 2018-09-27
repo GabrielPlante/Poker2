2d45350a81b1862b14b9370f99a5b6f05081352d
GabrielPlante <gabriel.plante@live.fr>
2018-09-27 10:56:35 +0200

    pre-slice3

--- src/Poker2/Enregistreur.java
+++ src/Poker2/Enregistreur.java
@@ -11,20 +11,20 @@ public class Enregistreur {
         while (paquet.size() < 10)
         {
             if (paquet.size() == 5) System.out.println("Entrer les cartes du joueur 2 :");
-            System.out.print("Carte n° "+(paquet.size()+(paquet.size()<5?1:-4))+" : ");//On passe au carte du deuxieme
-            Scanner sc_nombre = new Scanner(System.in);
-            String indice = sc_nombre.nextLine();
-            int nombre = convertisseur_indice(indice);
-            if (nombre == 0){
-                System.out.println("Valeur d'indice incorrecte !");
-                continue;
-            }
-            Carte carte = new Carte(nombre, "Pi");
+            System.out.println("Carte n° "+(paquet.size()+(paquet.size()<5?1:-4))+" : ");//On passe au carte du deuxieme
+            //---En premier l'indice de la carte---
+            int nombre = entrer_nombre();
+            if (nombre == 0) continue;
+            //---Ensuite la couleur de la carte---
+            String couleur = entrer_couleur();
+            if (couleur.isEmpty()) continue;
+            //---Et puis on la rentre dans le paquet---
+            Carte carte = new Carte(nombre, couleur);
             if (check_doublons(carte)){
                 System.out.println("Cette carte est déjà dans le paquet !");
                 continue;
             }
-            paquet.add(new Carte(nombre, "Pi"));
+            paquet.add(carte);
         }
     }
     public Enregistreur(ArrayList<Carte> paquet_brut)//Pour les tests uniquements
@@ -65,9 +65,35 @@ public class Enregistreur {
                 return true;
         return false;
     }
+    public boolean bonne_couleur(String couleur){
+        if (couleur.equals("Co") || couleur.equals("Pi"))
+            return true;
+        return false;
+    }
 
     public ArrayList<Carte> getPaquet(){
         return paquet;
+    }
+    public int entrer_nombre(){
+        System.out.print("Nombre : ");
+        Scanner sc_nombre = new Scanner(System.in);
+        String indice = sc_nombre.nextLine();
+        int nombre = convertisseur_indice(indice);
+        if (nombre == 0){
+            System.out.println("Valeur d'indice incorrecte !");
+            return 0;
+        }
+        return nombre;
+    }
+    public String entrer_couleur(){
+        System.out.print("Couleur :");
+        Scanner sc_couleur = new Scanner(System.in);
+        String couleur = sc_couleur.nextLine();
+        if (!bonne_couleur(couleur)){
+            System.out.println("Cette couleur n'est pas valide !");
+            return "";
+        }
+        return couleur;
     }
 
     private ArrayList<Carte> paquet = new ArrayList<Carte>();
